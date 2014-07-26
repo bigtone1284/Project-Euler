65f1988600b62fa51497c607ae480bca5a656eeb
bigtone1284 <bigtone1284@users.noreply.github.com>
2014-07-25 22:57:30 -0400

    Update tmath.py

--- solutions/tmath.py
+++ solutions/tmath.py
@@ -82,6 +82,25 @@ def get_prime_factors(n):
 
     return primeFactors	
 
+def primes(n): 
+	if n==2: return [2]
+	elif n<2: return []
+	s=range(3,n+1,2)
+	mroot = n ** 0.5
+	half=(n+1)/2-1
+	i=0
+	m=3
+	while m <= mroot:
+		if s[i]:
+			j=(m*m-3)/2
+			s[j]=0
+			while j<half:
+				s[j]=0
+				j+=m
+		i=i+1
+		m=2*i+3
+	return [2]+[x for x in s if x]
+
 def gen_Sieve_of_eratosthenes():
     """ Generate an infinite sequence of prime numbers.
     
